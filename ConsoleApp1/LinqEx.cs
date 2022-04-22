7928b9d0cc9b8ab6fabc1f742a85a6e02bfa335e
Far SM <fsm8678@gmail.com>
2022-04-22 20:31:17 +0400

    22/4/2022

---
+++ ConsoleApp1/LinqEx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class LinqEx
+    {
+        public static void Main()
+        {
+            int[] id = new int[4] { 1, 3, 2 , 4 };
+            var ids = from s in id
+                      where s > 1 && s < 4
+                      orderby s descending
+                      select s;
+
+            foreach (int i in ids)
+            {
+                Console.WriteLine(i);
+
+            }
+
+
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Eid {0}", id[i]);
+            //}
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
+   
---
+++ ConsoleApp1/LinqEx2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class LinqEx2
+    {
+        public static void Main()
+        {
+            EmpModel emp1 = new EmpModel();
+            emp1.EmpID = 121;
+            emp1.EmpName = "Sarita";
+            emp1.EmpSalary = 1000;
+
+            EmpModel emp2 = new EmpModel();
+            emp2.EmpID = 122;
+            emp2.EmpName = "Farheen";
+            emp2.EmpSalary = 3000;
+
+            EmpModel emp3 = new EmpModel();
+            emp3.EmpID = 123;
+            emp3.EmpName = "Sudhakar";
+            emp3.EmpSalary = 2000;
+
+            List<EmpModel> list = new List<EmpModel>();
+            list.Add(emp1);
+            list.Add(emp2);
+            list.Add(emp3);
+
+            //var emp = from e in list
+            //          where e.EmpSalary > 1000
+            //          select e;
+
+            //var emp = from e in list
+            //          where e.EmpName.Contains("S")
+            //          select e;
+
+            //var emp = list.Count();
+            //Console.Write(emp);
+
+            var emp = list.Where(x => x.EmpID > 1).ToList(); 
+
+            foreach (var item in emp)
+            {
+                Console.WriteLine(item.EmpName+ "," + item.EmpID );
+            }
+
+
+        }
+    }
+
+    public class EmpModel
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSalary { get; set; }
+
+    }
+}
